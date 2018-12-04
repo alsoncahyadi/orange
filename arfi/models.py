6c894108bb11758af1a8067e2db196eec5fbe7d2
alsoncahyadi <alson.cahyadi@gmail.com>
2018-12-04 19:53:11 +0700

    first commit

---
+++ arfi/models.py
@@ -0,0 +1,11 @@
+from django.db import models as m
+
+# Create your models here.
+class Question(m.Model):
+  text = m.CharField(max_length=200)
+  pub_date = m.DateTimeField('date published')
+
+class Choice(m.Model):
+  question = m.ForeignKey(Question, on_delete=m.CASCADE)
+  choice_text = m.CharField(max_length=200)
+  votes = m.IntegerField(default=0)
