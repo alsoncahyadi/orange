cb9ac2a95f1b3fa6eecb634b120e4ff09173c298
alsoncahyadi <alson.cahyadi@gmail.com>
2018-12-13 01:44:00 +0700

    Added purchase_order_id, added indexes

--- arfi/models.py
+++ arfi/models.py
@@ -11,8 +11,8 @@ class Client(m.Model):
     return "{}: {} (ID: {})".format(self.__class__.__name__, self.name, self.id)
 
   id = m.AutoField(verbose_name="Client ID", primary_key=True)
-  name = m.CharField(max_length=200, verbose_name="Nama Client")
-  address = m.CharField(max_length=500, verbose_name="Alamat")
+  name = m.CharField(max_length=200, verbose_name="Nama Client", db_index=True)
+  address = m.CharField(max_length=500, verbose_name="Alamat", db_index=True)
 
 
 class Mandor(m.Model):
@@ -24,9 +24,9 @@ class Mandor(m.Model):
     return "{}: {} (ID: {})".format(self.__class__.__name__, self.name, self.id)
 
   id = m.AutoField(verbose_name="Mandor ID", primary_key=True)
-  name = m.CharField(max_length=200, verbose_name="Nama Mandor")
-  address = m.CharField(max_length=500, verbose_name="Alamat")
-  phone = m.CharField(max_length=20, verbose_name="No. HP")
+  name = m.CharField(max_length=200, verbose_name="Nama Mandor", db_index=True)
+  address = m.CharField(max_length=500, verbose_name="Alamat", db_index=True)
+  phone = m.CharField(max_length=20, verbose_name="No. HP", db_index=True)
 
 
 class JobOrder(m.Model):
@@ -35,7 +35,7 @@ class JobOrder(m.Model):
     verbose_name_plural = "Job Orders"
 
   id = m.AutoField(verbose_name="No.Order", primary_key=True)
-  date = m.DateTimeField(verbose_name="Tanggal", )
+  date = m.DateTimeField(verbose_name="Tanggal", db_index=True)
   mandor_id = m.ForeignKey(Mandor, verbose_name="Mandor ID", on_delete=m.DO_NOTHING)
   # mandor_name
   job_info = m.CharField(max_length=1000, verbose_name="Uraian Pekerjaan")
@@ -47,11 +47,11 @@ class ServiceOrder(m.Model):
     verbose_name_plural = "Service Orders"
 
   id = m.AutoField(verbose_name="No.Order", primary_key=True)
-  date = m.DateTimeField(verbose_name="Tanggal")
+  date = m.DateTimeField(verbose_name="Tanggal", db_index=True)
   client_id = m.ForeignKey(Client, verbose_name="Client ID", on_delete=m.DO_NOTHING)
   # client_name
-  project_name = m.CharField(max_length=200, verbose_name="Nama Proyek")
-  address = m.CharField(max_length=500, verbose_name="Alamat")
+  project_name = m.CharField(max_length=200, verbose_name="Nama Proyek", db_index=True)
+  address = m.CharField(max_length=500, verbose_name="Alamat", db_index=True)
   jobs = m.ManyToManyField(JobOrder, verbose_name="List Pekerjaan")
 
 
@@ -62,11 +62,11 @@ class ServiceBill(m.Model):
 
   id = m.AutoField(verbose_name="No.Order", primary_key=True)
   service_order_id = m.OneToOneField(ServiceOrder, verbose_name="Service Order ID", on_delete=m.DO_NOTHING, db_index=True)
-  date = m.DateTimeField(verbose_name="Tanggal")
+  date = m.DateTimeField(verbose_name="Tanggal", db_index=True)
   client_id = m.ForeignKey(Client, verbose_name="Client ID", on_delete=m.DO_NOTHING, default=None)
   # client_name
-  project_name = m.CharField(max_length=200, verbose_name="Nama Proyek")
-  address = m.CharField(max_length=500, verbose_name="Alamat")
+  project_name = m.CharField(max_length=200, verbose_name="Nama Proyek", db_index=True)
+  address = m.CharField(max_length=500, verbose_name="Alamat", db_index=True)
   jobs = m.ManyToManyField(JobOrder, verbose_name="List Pekerjaan")
   price = m.BigIntegerField(verbose_name="Harga")
   total_price = m.BigIntegerField(verbose_name="Total Harga")
@@ -79,9 +79,9 @@ class BudgetPlan(m.Model):
 
   id = m.AutoField(verbose_name="No.Order", primary_key=True)
   service_order_id = m.OneToOneField(ServiceOrder, verbose_name="Service Order ID", on_delete=m.DO_NOTHING, db_index=True)
-  job_info = m.CharField(max_length=1000, verbose_name="Uraian Pekerjaan")
-  volume = m.CharField(max_length=200)
-  unit = m.CharField(max_length=20)
+  job_info = m.CharField(max_length=1000, verbose_name="Uraian Pekerjaan", db_index=True)
+  volume = m.CharField(max_length=200, db_index=True)
+  unit = m.CharField(max_length=20, db_index=True)
   price = m.BigIntegerField(verbose_name="Harga")
   amount = m.BigIntegerField(verbose_name="Jumlah")
   # total
@@ -99,8 +99,8 @@ class Transaction(m.Model):
   item = m.ForeignKey('Item', verbose_name="Item", on_delete=m.DO_NOTHING)
   receiving_report = m.ForeignKey('ReceivingReport', verbose_name="Receiving Report", on_delete=m.DO_NOTHING)
   quantity = m.BigIntegerField(verbose_name="Quantity", default=1)
-  created_at = m.DateTimeField(auto_now_add=True)
-  updated_at = m.DateTimeField(auto_now=True)
+  created_at = m.DateTimeField(auto_now_add=True, db_index=True)
+  updated_at = m.DateTimeField(auto_now=True, db_index=True)
 
 
 class Item(m.Model):
@@ -111,8 +111,8 @@ class Item(m.Model):
   def __str__(self):
     return "{}: {} (ID: {})".format(self.__class__.__name__, self.name, self.id)
   
-  updated_at = m.DateTimeField(verbose_name="Tanggal Diperbaharui", auto_now=True)
-  name = m.CharField(verbose_name="Nama Barang", max_length=200, default="")
+  updated_at = m.DateTimeField(verbose_name="Tanggal Diperbaharui", auto_now=True, db_index=True)
+  name = m.CharField(verbose_name="Nama Barang", max_length=200, default="", db_index=True)
   quantity = m.BigIntegerField(verbose_name="Quantity", default=0)
   price = m.BigIntegerField(verbose_name="Harga", default=0)
 
@@ -123,7 +123,7 @@ class PurchaseOrder(m.Model):
     verbose_name_plural = "Purchase Orders"
 
   id = m.AutoField(verbose_name="PO. Number", primary_key=True)
-  date = m.DateTimeField(verbose_name="Tanggal")
+  date = m.DateTimeField(verbose_name="Tanggal", db_index=True)
   supplier_id = m.ForeignKey('Supplier', verbose_name="Supplier ID", on_delete=m.DO_NOTHING)
   # supplier_name
   # item_number, item_name
@@ -135,10 +135,10 @@ class ReceivingReport(m.Model):
     verbose_name = "Receiving Report"
     verbose_name_plural = "Receiving Reports"
 
-  id = m.AutoField(verbose_name="PO. Number", primary_key=True)
+  purchase_order_id = m.OneToOneField(PurchaseOrder, verbose_name="Purchase Order ID", on_delete=m.DO_NOTHING, default=None, db_index=True)
   supplier_id = m.ForeignKey('Supplier', verbose_name="Supplier ID", on_delete=m.DO_NOTHING)
   # supplier_name
-  date = m.DateTimeField(verbose_name="Tanggal")
+  date = m.DateTimeField(verbose_name="Tanggal", db_index=True)
   items = m.ManyToManyField(Item, verbose_name="Items list", through=Transaction)
 
 
@@ -147,10 +147,11 @@ class PaymentReceipt(m.Model):
     verbose_name = "Payment Receipt"
     verbose_name_plural = "Payment Receipts"
 
-  date = m.DateTimeField(verbose_name="Tanggal")
+  purchase_order_id = m.OneToOneField(PurchaseOrder, verbose_name="Purchase Order ID", on_delete=m.DO_NOTHING, default=None, db_index=True)
+  date = m.DateTimeField(verbose_name="Tanggal", db_index=True)
   bill = m.BigIntegerField(verbose_name="Tagihan")
   total = m.BigIntegerField(verbose_name="Total")
-  confirmation = m.CharField(max_length=200)
+  confirmation = m.CharField(max_length=200, db_index=True)
 
 
 class Supplier(m.Model):
@@ -161,6 +162,6 @@ class Supplier(m.Model):
   def __str__(self):
     return "{}: {} (ID: {})".format(self.__class__.__name__, self.name, self.id)
 
-  name = m.CharField(max_length=200, verbose_name="Nama Supplier")
-  address = m.CharField(verbose_name="Alamat", max_length=200)
-  phone = m.CharField(max_length=20, verbose_name="No. HP")
+  name = m.CharField(max_length=200, verbose_name="Nama Supplier", db_index=True)
+  address = m.CharField(verbose_name="Alamat", max_length=200, db_index=True)
+  phone = m.CharField(max_length=20, verbose_name="No. HP", db_index=True)
