4c68cf856b1dab0a15ca292638a84f42aa6ae548
alsoncahyadi <alson.cahyadi@gmail.com>
2018-12-12 07:49:54 +0700

    Fixed index always redirect to client, added budget_plan to url

--- arfi/urls.py
+++ arfi/urls.py
@@ -4,10 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # Revenue
     path('clients', views.clients_view, name='clients'),
     path('service_orders', views.service_orders_view, name='service_orders_view'),
     path('job_orders', views.job_orders_view, name='job_orders_view'),
     path('service_bills', views.service_bills_view, name='service_bills_view'),
+    path('budget_plans', views.budget_plans_view, name='budget_plans_view'),
+    # Expenditure
     path('purchase_orders', views.purchase_orders_view, name='purchase_orders_view'),
     path('items', views.items_view, name='items_view'),
     path('receiving_reports', views.receiving_reports_view, name='receiving_reports_view'),
